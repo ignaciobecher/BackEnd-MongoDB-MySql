2a649452a42987688905fed1b1cd731cf70ce044
Ignacio <ignacio.becher@gmail.com>
2023-03-13 12:15:01 -0300

    Files ok

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("storage")); //permitir que el navegador muestre los archivos del storage
 app.listen(port, () => {
   console.log("App en linea en puerto: " + port);
 });
--- controllers/storage.js
+++ controllers/storage.js
@@ -1,5 +1,6 @@
 //FORMA CORRECTA DE IMPORTAR MODELO
 const { storageModel } = require("../models");
+const PUBLIC_URL = process.env.PUBLIC_URL;
 
 const getItems = async (req, res) => {
   const data = await storageModel.find({});
@@ -8,9 +9,14 @@ const getItems = async (req, res) => {
 const getItem = (req, res) => {};
 
 const createItem = async (req, res) => {
-  const { body } = req; //cuando la propiedad se llama como la variable, aplico desestructuracion
-  console.log(body);
-  const data = await storageModel.create(body);
+  const { body, file } = req; //cuando la propiedad se llama como la variable, aplico desestructuracion
+  console.log(file);
+  const fileData = {
+    filename: file.filename,
+    url: `${PUBLIC_URL}/${file.filename}`,
+  };
+
+  const data = await storageModel.create(fileData);
   res.send({ data });
 };
 const updateItem = (req, res) => {};
